09dfd8fb360222dd16fd387642a13d5e7e4235be
Chuefeng Vue <chuefe.vue@gmail.com>
2020-07-23 09:38:19 -0500

    Awaiting Extended Evaluation

---
+++ assets/javascripts/Doctor.js
@@ -0,0 +1,12 @@
+class Doctor extends Traveler {
+    constructor(name) {
+        super(name)
+    }
+    heal(traveler) {
+        if (traveler.isHealthy === false) {
+            traveler.isHealthy = true
+        } else {
+            //This Traveler is in good health!
+        }
+    }
+}
